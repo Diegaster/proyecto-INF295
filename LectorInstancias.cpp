859a865ac2f8b94a727f230aa01f499d085f560d
Diego Muñoz <117866697+Diegaster@users.noreply.github.com>
2026-06-10 14:04:47 -0400

    Create LectorInstancias.cpp

---
+++ LectorInstancias.cpp
@@ -0,0 +1,13 @@
+#include "../include/LectorInstancias.h"
+
+Instancia LectorInstancias::leer(const std::string& archivo)
+{
+    Instancia instancia;
+
+    // TODO:
+    // Leer archivo
+    // Parsear depósitos
+    // Parsear clientes
+
+    return instancia;
+}
