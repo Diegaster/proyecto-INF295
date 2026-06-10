50a0d771dce7cc5b1d900a24cc5b604c60d71e32
Diego Muñoz <117866697+Diegaster@users.noreply.github.com>
2026-06-10 14:04:23 -0400

    Create LectorInstancias.h

---
+++ LectorInstancias.h
@@ -0,0 +1,15 @@
+#ifndef LECTOR_INSTANCIAS_H
+#define LECTOR_INSTANCIAS_H
+
+#include "Instancia.h"
+#include <string>
+
+class LectorInstancias {
+
+public:
+
+    static Instancia leer(const std::string& archivo);
+
+};
+
+#endif
