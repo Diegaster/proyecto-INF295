ac469c680a1ca2c350c876fe95c9e2069f67d5ab
Diego Munoz <munozcarrillo.diego@gmail.com>
2026-06-19 21:13:52 -0400

    Añadir relocate

--- src/TabuSearch.cpp
+++ src/TabuSearch.cpp
@@ -23,6 +23,47 @@ static Solucion aplicarSwap(
     return vecino;
 }
 
+static Solucion aplicarRelocate(
+    const Solucion& original,
+    int rutaOrigen,
+    int posOrigen,
+    int rutaDestino,
+    int posDestino
+)
+{
+    Solucion vecino = original;
+
+    int cliente =
+        vecino.rutas[rutaOrigen]
+              .clientes[posOrigen];
+
+    vecino.rutas[rutaOrigen]
+          .clientes.erase(
+              vecino.rutas[rutaOrigen]
+                    .clientes.begin()
+              + posOrigen
+          );
+
+    if(posDestino >
+       (int)vecino.rutas[rutaDestino]
+                      .clientes.size())
+    {
+        posDestino =
+            vecino.rutas[rutaDestino]
+                  .clientes.size();
+    }
+
+    vecino.rutas[rutaDestino]
+          .clientes.insert(
+              vecino.rutas[rutaDestino]
+                    .clientes.begin()
+              + posDestino,
+              cliente
+          );
+
+    return vecino;
+}
+
 static bool esTabu(
     const MovimientoTabu& mov,
     const std::deque<MovimientoTabu>& lista
@@ -77,6 +118,10 @@ Solucion TabuSearch::optimizar(
 
         bool encontrado = false;
 
+        //--------------------------------------------------
+        // SWAP
+        //--------------------------------------------------
+
         for(int r1 = 0;
             r1 < (int)actual.rutas.size();
             r1++)
@@ -117,6 +162,112 @@ Solucion TabuSearch::optimizar(
 
                         Solucion vecino =
                             aplicarSwap(
+                                actual,
+                                r1,
+                                p1,
+                                r2,
+                                p2
+                            );
+
+                        ResultadoEvaluacion eval =
+                            Evaluador::evaluar(
+                                instancia,
+                                vecino
+                            );
+
+                        bool tabu =
+                            esTabu(
+                                mov,
+                                tabuList
+                            );
+
+                        bool aspiracion =
+                            eval.fitness <
+                            mejorGlobalEval.fitness;
+
+                        if(
+                            tabu &&
+                            !aspiracion
+                        )
+                        {
+                            continue;
+                        }
+
+                        if(
+                            eval.fitness <
+                            mejorVecinoEval.fitness
+                        )
+                        {
+                            mejorVecino =
+                                vecino;
+
+                            mejorVecinoEval =
+                                eval;
+
+                            mejorMovimiento =
+                                mov;
+
+                            encontrado =
+                                true;
+                        }
+                    }
+                }
+            }
+        }
+
+        //--------------------------------------------------
+        // RELOCATE
+        //--------------------------------------------------
+
+        for(int r1 = 0;
+            r1 < (int)actual.rutas.size();
+            r1++)
+        {
+            if(
+                actual.rutas[r1]
+                      .clientes.size()
+                <= 1
+            )
+            {
+                continue;
+            }
+
+            for(int p1 = 0;
+                p1 < (int)actual.rutas[r1].clientes.size();
+                p1++)
+            {
+                int cliente =
+                    actual.rutas[r1]
+                          .clientes[p1];
+
+                for(int r2 = 0;
+                    r2 < (int)actual.rutas.size();
+                    r2++)
+                {
+                    for(int p2 = 0;
+                        p2 <= (int)actual.rutas[r2]
+                                       .clientes.size();
+                        p2++)
+                    {
+                        if(
+                            r1 == r2 &&
+                            (p2 == p1 ||
+                             p2 == p1 + 1)
+                        )
+                        {
+                            continue;
+                        }
+
+                        MovimientoTabu mov;
+
+                        mov.clienteA =
+                            cliente;
+
+                        mov.clienteB =
+                            -1;
+
+                        Solucion vecino =
+                            aplicarRelocate(
                                 actual,
                                 r1,
                                 p1,
