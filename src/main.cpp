14217791160fd3ccac013ebef03dc87c96721fbe
Diego Munoz <munozcarrillo.diego@gmail.com>
2026-06-19 22:45:00 -0400

    Inclusion de data al azar + Soluciones iniciales de Greedy

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,28 @@
 #include <filesystem>
 #include <vector>
 #include <algorithm>
+#include <fstream>
+#include <chrono>
 
-int main(){
+int main()
+{
+    //----------------------------------------
+    // Configuración ejecución
+    //----------------------------------------
+
+    char modo;
+
+    std::cout
+        << "Modo de ejecucion:\n"
+        << "G = Solo Greedy\n"
+        << "T = Solo Tabu (desde solucion Greedy)\n"
+        << "A = Ambos\n"
+        << "Opcion: ";
+
+    std::cin >> modo;
+
+    modo = std::toupper(modo);
+
     //----------------------------------------
     // Configuración Tabu
     //----------------------------------------
@@ -51,14 +71,49 @@ int main(){
          opcion == 'Y');
 
     if(
+        modo != 'G' &&
         !config.usarSwap &&
         !config.usarRelocate
-    ){
+    )
+    {
         std::cout
             << "Debe activar al menos un movimiento.\n";
 
         return 1;
     }
+
+    //----------------------------------------
+    // Directorios
+    //----------------------------------------
+
+    std::filesystem::create_directories(
+        "output"
+    );
+
+    //----------------------------------------
+    // CSV consolidado
+    //----------------------------------------
+
+    std::ofstream csv(
+        "output/resultados.csv"
+    );
+
+    csv
+    << "Instancia,"
+    << "Clientes,"
+    << "Modo,"
+    << "Iteraciones,"
+    << "TabuTenure,"
+    << "Swap,"
+    << "Relocate,"
+    << "FitnessGreedy,"
+    << "FitnessTabu,"
+    << "Mejora,"
+    << "PenalizacionGreedy,"
+    << "PenalizacionTabu,"
+    << "VehiculosGreedy,"
+    << "VehiculosTabu,"
+    << "TiempoTabuMs\n";
 
     //----------------------------------------
     // Buscar instancias
@@ -67,7 +122,10 @@ int main(){
     std::vector<std::string> archivos;
 
     for(const auto& entry :
-        std::filesystem::directory_iterator("data")){
+        std::filesystem::directory_iterator(
+            "data"
+        ))
+    {
         if(entry.is_regular_file())
         {
             archivos.push_back(
@@ -82,14 +140,34 @@ int main(){
     );
 
     //----------------------------------------
-    // Resolver todas las instancias
+    // Procesar instancias
     //----------------------------------------
 
-    for(const auto& archivo : archivos){
+    for(const auto& archivo : archivos)
+    {
+        Instancia instancia =
+            LectorInstancias::leer(
+                archivo
+            );
+
         std::string nombre =
-            std::filesystem::path(archivo)
-            .stem()
-            .string();
+            std::filesystem::path(
+                archivo
+            ).stem().string();
+
+        int cantidadClientes =
+            instancia.clientes.size();
+
+        std::string carpeta =
+            "output/" +
+            std::to_string(
+                cantidadClientes
+            ) +
+            "_clientes";
+
+        std::filesystem::create_directories(
+            carpeta
+        );
 
         std::cout
             << "\n====================================\n";
@@ -100,87 +178,139 @@ int main(){
             << "\n";
 
         std::cout
-            << "====================================\n";
-
-        //------------------------------------
-        // Leer instancia
-        //------------------------------------
-
-        Instancia instancia =
-            LectorInstancias::leer(
-                archivo
-            );
+            << "Clientes: "
+            << cantidadClientes
+            << "\n";
 
         //------------------------------------
         // Greedy
         //------------------------------------
 
-        Solucion solucion =
+        Solucion solucionGreedy =
             ConstructorGreedy::construir(
                 instancia
             );
 
-        ResultadoEvaluacion resultado =
+        ResultadoEvaluacion resultadoGreedy =
             Evaluador::evaluar(
                 instancia,
-                solucion
+                solucionGreedy
             );
 
-        EscritorSolucion::escribir(
-            "output/" +
-            nombre +
-            "_greedy.txt",
-            instancia,
-            solucion,
-            resultado
-        );
-
-        //------------------------------------
-        // Tabu Search
-        //------------------------------------
-
-        Solucion tabu =
-            TabuSearch::optimizar(
+        if(
+            modo == 'G' ||
+            modo == 'A'
+        )
+        {
+            EscritorSolucion::escribir(
+                carpeta +
+                "/" +
+                nombre +
+                "_greedy.txt",
                 instancia,
-                solucion,
-                config
+                solucionGreedy,
+                resultadoGreedy
             );
+        }
 
-        ResultadoEvaluacion resultadoTabu =
-            Evaluador::evaluar(
+        //------------------------------------
+        // Tabu
+        //------------------------------------
+
+        ResultadoEvaluacion resultadoTabu;
+        resultadoTabu.fitness =
+            resultadoGreedy.fitness;
+
+        resultadoTabu.penalizacionTotal =
+            resultadoGreedy.penalizacionTotal;
+
+        resultadoTabu.vehiculosUsados =
+            resultadoGreedy.vehiculosUsados;
+
+        double tiempoTabuMs = 0.0;
+
+        if(
+            modo == 'T' ||
+            modo == 'A'
+        )
+        {
+            auto inicio =
+                std::chrono::high_resolution_clock::now();
+
+            Solucion solucionTabu =
+                TabuSearch::optimizar(
+                    instancia,
+                    solucionGreedy,
+                    config
+                );
+
+            auto fin =
+                std::chrono::high_resolution_clock::now();
+
+            tiempoTabuMs =
+                std::chrono::duration<double,
+                std::milli>(
+                    fin - inicio
+                ).count();
+
+            resultadoTabu =
+                Evaluador::evaluar(
+                    instancia,
+                    solucionTabu
+                );
+
+            EscritorSolucion::escribir(
+                carpeta +
+                "/" +
+                nombre +
+                "_tabu.txt",
                 instancia,
-                tabu
+                solucionTabu,
+                resultadoTabu
             );
 
-        EscritorSolucion::escribir(
-            "output/" +
-            nombre +
-            "_tabu.txt",
-            instancia,
-            tabu,
-            resultadoTabu
-        );
+            std::cout
+                << "Greedy Fitness: "
+                << resultadoGreedy.fitness
+                << "\n";
+
+            std::cout
+                << "Tabu Fitness: "
+                << resultadoTabu.fitness
+                << "\n";
+
+            std::cout
+                << "Mejora: "
+                << resultadoGreedy.fitness -
+                   resultadoTabu.fitness
+                << "\n";
+        }
 
         //------------------------------------
-        // Resumen
+        // CSV
         //------------------------------------
 
-        std::cout
-            << "Greedy Fitness: "
-            << resultado.fitness
-            << "\n";
-
-        std::cout
-            << "Tabu Fitness: "
-            << resultadoTabu.fitness
-            << "\n";
-
-        std::cout
-            << "Mejora: "
-            << resultado.fitness -
-               resultadoTabu.fitness
-            << "\n";
+        csv
+        << nombre << ","
+        << cantidadClientes << ","
+        << modo << ","
+        << config.maxIteraciones << ","
+        << config.tabuTenure << ","
+        << (config.usarSwap ? 1 : 0) << ","
+        << (config.usarRelocate ? 1 : 0) << ","
+        << resultadoGreedy.fitness << ","
+        << resultadoTabu.fitness << ","
+        << (resultadoGreedy.fitness -
+            resultadoTabu.fitness) << ","
+        << resultadoGreedy.penalizacionTotal << ","
+        << resultadoTabu.penalizacionTotal << ","
+        << resultadoGreedy.vehiculosUsados << ","
+        << resultadoTabu.vehiculosUsados << ","
+        << tiempoTabuMs
+        << "\n";
     }
+
+    csv.close();
 
     std::cout
         << "\nTodas las instancias fueron procesadas.\n";
